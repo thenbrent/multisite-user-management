89f3af45a0942423ae0c4e4fefb1d274844d6fd9
Brent Shepherd <thenbrent@gmail.com>
2011-03-29 15:30:25 +0700

    tagging v0.7 and deploying to wp.org svn repo

--- deploy.sh
+++ deploy.sh
@@ -33,49 +33,39 @@ if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exitin
 
 echo "Versions match in readme.txt and PHP file. Let's proceed..."
 
-# change into the git dir and get a commit message
 cd $GITPATH
 echo -e "Enter a commit message for this new version: \c"
 read COMMITMSG
 git commit -am "$COMMITMSG"
 
-# Create a new tag and commit it :)
 echo "Tagging new version in git"
 git tag -a "$NEWVERSION1" -m "$COMMITMSG"
 
-# push to origin
-echo "Push latest commit to origin, with tags"
-#git push origin master
-#git push origin master --tags
+echo "Pushing latest commit to origin, with tags"
+git push origin master
+git push origin master --tags
 
-#Create temporary SVN repo
 echo 
 echo "Creating local copy of SVN repo ..."
 svn co $SVNURL $SVNPATH
 
-# Export git contents to svn directory
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
-# Make sure SVN is ignore this file and other files for git/github
 echo "Ignoring github specific files and deployment script"
 svn propset svn:ignore "deploy.sh" "$SVNPATH/"
 svn propset svn:ignore "readme.md" "$SVNPATH/"
 
-# Change to SVN dir and commit changes
 echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk
-#svn commit --username=$SVNUSER -m "$COMMITMSG"
+svn commit --username=$SVNUSER -m "$COMMITMSG"
 
-# Create a new tag and commit it :)
-echo "Creating new SVN tag"
+echo "Creating new SVN tag & committing it"
 cd $SVNPATH
 svn copy trunk/ tags/$NEWVERSION1
-#svn commit --username=$SVNUSER -m "Updating tag to $NEWVERSION1"
+svn commit --username=$SVNUSER -m "Updating tag to $NEWVERSION1"
 
-# Remove temporary SVN directory
 echo "Removing temporary directory $SVNPATH"
 #rm -fr $SVNPATH/
 
-# Ce Fin
 echo "*** FIN ***"
