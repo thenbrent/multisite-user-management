a58e399dd7069ff2f862662335b43654bf49de47
Brent Shepherd <thenbrent@gmail.com>
2011-04-07 13:53:29 +0700

    Updated version of deployment script with proper SVN ignore command

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
-# The difference is that this script lives in the plugin's git repo.
+# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
 # main config
 PLUGINSLUG="multisite-user-management"
@@ -54,10 +54,10 @@ echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
 echo "Ignoring github specific files and deployment script"
-svn propset svn:ignore "deploy.sh" "$SVNPATH/trunk/"
-svn propset svn:ignore "README.md" "$SVNPATH/trunk/"
-svn propset svn:ignore ".git" "$SVNPATH/trunk/"
-svn propset svn:ignore ".gitignore" "$SVNPATH/trunk/"
+svn propset svn:ignore "deploy.sh
+README.md
+.git
+.gitignore" "$SVNPATH/trunk/"
 
 echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk/
