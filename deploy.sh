938bf5e29d346f84a4dca382e4f7737a2646856a
Terence Eden <github.com@shkspr.mobi>
2015-10-29 15:08:10 +0000

    Trailing slash needed
    Fixes #22

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/multisite-user-management/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNURL="http://plugins.svn.wordpress.org/multisite-user-management/" # Remote SVN repo on wordpress.org, with trailing slash
 SVNUSER="thenbrent" # your svn username
 
 
